fd13395ce9d6759235012f6bb751c486e1f787f1
awschnyi <31037540+awschnyi@users.noreply.github.com>
2019-09-26 21:00:35 +0800

    up

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. 20190926
 class Topic(models.Model):
 	"""user's learning topic"""
 	text = models.CharField(max_length=200)
